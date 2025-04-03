d5635db4c2b63f514bf3fb740081194a20537070
Astramast <azerqsdfazerqsdg@gmail.com>
2025-04-03 16:13:11 +0200

    Thu  3 Apr 16:13:11 CEST 2025

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import pygame
 
 pygame.init()
-time_font = pygame.font.Font("assets/GlacialIndifference-Regular.otf", 90)
-talks_font = pygame.font.Font("assets/GlacialIndifference-Regular.otf", 66)
+times_font = pygame.font.Font("assets/LEMONMILK-Medium.otf", 60)
+talks_font = pygame.font.Font("assets/GlacialIndifference-Bold.otf", 60)
 
-date = input("Date of the event >").upper()
-time = input("Time of the event >").upper()
-talk_names = input("Talk names >")
-output = input("Output file name >")
+output = 'a'
+
+text1 = "Des conférences sur la technologie et"
+text2 = "l'informatique tous les premiers"
+text21 = "lundis du mois à 18h30"
+text3 = "Plus d'infos sur : https://urlab.be/sm"
+text4 = "Nous contacter ? contact@urlab.be"
 
 base_image = pygame.image.load("assets/Smartmonday brain.png")
 size = base_image.get_size()
 base_surface = pygame.Surface(size)
 window = pygame.display.set_mode(size)
 
-date = f"{date} • {time}"
-date_text = time_font.render(date, True, (0, 0, 0))
-talk_text = talks_font.render(talk_names, True, (0, 0, 0))
+date = f"{text1}"
+date_text = times_font.render(date, True, (0, 0, 0))
+text2_text = times_font.render(text2, True, (0, 0, 0))
+text21_text = times_font.render(text21, True, (0, 0, 0))
+text3_text = talks_font.render(text3, True, (0, 0, 0))
+text4_text = talks_font.render(text4, True, (0, 0, 0))
 
 base_image.blit(base_image, (0, 0))
 base_image.blit(date_text, (size[0] // 2 - date_text.get_size()[0] // 2, 1600))
-base_image.blit(talk_text, (size[0] // 2 - talk_text.get_size()[0] // 2, 1750))
+base_image.blit(text2_text, (size[0] // 2 - text2_text.get_size()[0] // 2, 1680))
+base_image.blit(text21_text, (size[0] // 2 - text21_text.get_size()[0] // 2, 1760))
+base_image.blit(text3_text, (size[0] // 2 - text3_text.get_size()[0] // 2, 1950))
+base_image.blit(text4_text, (size[0] // 2 - text4_text.get_size()[0] // 2, 2020))
 
 pygame.image.save(base_image, output)
 
@@ -30,3 +39,4 @@ pygame.image.save(base_image, output)
 #         if event.type == pygame.QUIT:
 #             pygame.quit()
 #             break
+
